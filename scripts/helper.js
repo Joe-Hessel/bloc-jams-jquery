01beeead0c836293ba22c43a28f742e9f169d945
Joe Hessel <socialandvideomarketing@gmail.com>
2019-09-03 09:33:21 -0400

    Total time fixed

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,7 +1,10 @@
-//helper//
 class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
+
+  const duration = player.currentlyPlaying.duration;
+  $('#time-control .total-time').text( duration );
   }
 }
+
 const helper = new Helper();
